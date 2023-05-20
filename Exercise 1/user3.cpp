79a2fe0c67d38b52540efbfbb8ac2890e28fb286
Muhammad Ali Ahson <105967134+MuhammadAliAhson@users.noreply.github.com>
2023-05-21 01:52:56 +0500

    Create user3.cpp

---
+++ Exercise 1/user3.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <wait.h>
+
+using namespace std;
+
+int main(int narg, char **arg)
+{
+    string sentence;
+
+    int cd = (arg[1][0] - '0');
+
+    if (cd == 0)
+    {
+    // creating and opening shared memory
+        key_t marker = ftok("Chat box", 65);
+        int memory = shmget(marker, 1024, 0666 | IPC_CREAT);
+      // adding new text to new neext location
+        char *text = (char *)shmat(memory, NULL, 0);
+        text += strlen(text);
+        // taking new input
+        cout << "enter input for person 3" << endl;
+        getline(cin, sentence);
+        if (sentence == "exit")
+            exit(0);
+        string a = "user3 : " + sentence + "\n";
+        strcpy(text, a.c_str());
+        shmdt(text);
+    }
+    else
+    {
+
+        cout << "-------------------------------->>>>>>READING FROM USER 3" << endl;
+        key_t marker = ftok("Chat box", 65);
+        int memory = shmget(marker, 1024, 0);
+        char *text = (char *)shmat(memory, NULL, 0);
+        cout<<text;
+        shmdt(text);
+        // shmctl(memory, IPC_RMID, NULL);
+    }
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+        execlp("./run", "run", NULL);
+        exit(0);
+    }
+    else
+        wait(NULL);
+}
